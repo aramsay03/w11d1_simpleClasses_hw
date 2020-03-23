5443a0cfac91383b9f6c9b6dfd07587975fba4c7
aramsay03 <alan@aramsay.co.uk>
2020-03-23 17:17:46 +0000

    Calculator subtract test passing 2 passing tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,10 +6,13 @@ public class Calculator {
     public Calculator(int value1, int value2) {
         this.value1 = value1;
         this.value2 = value2;
-
     }
 
     public int add() {
         return value1 + value2;
+    }
+
+    public int subtract() {
+        return value1 - value2;
     }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,5 +1,5 @@
+import org.junit.Assert;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class TestCalculator {
@@ -8,5 +8,11 @@ public class TestCalculator {
     public void addValues(){
         Calculator calculator = new Calculator(14, 4);
         assertEquals( 18, calculator.add());
+    }
+
+    @Test
+    public void subtractValues(){
+        Calculator calculator = new Calculator(6, 2);
+        assertEquals(4, calculator.subtract());
     }
 }
