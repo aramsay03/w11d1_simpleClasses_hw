370781a3142f62f14a6e335b4421889682c329fb
aramsay03 <alan@aramsay.co.uk>
2020-03-23 23:17:42 +0000

    Calculator divide test passing 4 passing tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,23 +1,26 @@
 public class Calculator {
 
-    private int value1;
-    private int value2;
+    private double value1;
+    private double value2;
 
-    public Calculator(int value1, int value2) {
+    public Calculator(double value1, double value2) {
         this.value1 = value1;
         this.value2 = value2;
     }
 
-    public int add() {
+    public double add() {
         return value1 + value2;
     }
 
-    public int subtract() {
+    public double subtract() {
         return value1 - value2;
     }
 
-    public int multiply() {
+    public double multiply() {
         return value1 * value2;
     }
 
+    public double divide() {
+        return value1 / value2;
+    }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -7,18 +7,24 @@ public class TestCalculator {
     @Test
     public void addValues() {
         Calculator calculator = new Calculator(14, 4);
-        assertEquals( 18, calculator.add());
+        assertEquals( 18, calculator.add(), 0);
     }
 
     @Test
     public void subtractValues() {
         Calculator calculator = new Calculator(6, 2);
-        assertEquals(4, calculator.subtract());
+        assertEquals(4, calculator.subtract(), 0);
     }
 
     @Test
     public void multiplyValues() {
         Calculator calculator = new Calculator(3, 5);
-        assertEquals(15, calculator.multiply());
+        assertEquals(15, calculator.multiply(), 0);
+    }
+
+    @Test
+    public void divideValues() {
+        Calculator calculator = new Calculator(25, 8);
+        assertEquals(3.125, calculator.divide(),0.001);
     }
 }
