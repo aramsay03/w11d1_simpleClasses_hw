18b2bd7c0bdb43031151199f2319453dab674477
aramsay03 <alan@aramsay.co.uk>
2020-03-23 16:59:25 +0000

    Calculator add test passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,15 @@
 public class Calculator {
+
+    private int value1;
+    private int value2;
+
+    public Calculator(int value1, int value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+
+    }
+
+    public int add() {
+        return value1 + value2;
+    }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestCalculator {
+
+    @Test
+    public void addValues(){
+        Calculator calculator = new Calculator(14, 4);
+        assertEquals( 18, calculator.add());
+    }
 }
